e6288892516d2a73d44c6d0d9a052352c0758527
CirinMok <cirinmok@gmail.com>
2026-05-10 21:12:47 +0800

    Delete scripts directory

--- scripts/predict_fasta.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Predict enzyme probabilities from a FASTA file with a trained sxRaep model."""
-
-from sxraep.cli import main
-
-
-if __name__ == "__main__":
-    main(["predict"])
